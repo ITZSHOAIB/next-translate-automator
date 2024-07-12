fbddb778264c2ce61c007cb9cd947f77d7d3008e
Sohab Sk <sohab.sk@skillsoft.com>
2024-07-12 21:51:05 +0530

    updated processJSXText

--- src/fileProcessor/jsxHandlers.js
+++ src/fileProcessor/jsxHandlers.js
@@ -42,64 +42,13 @@ const processJSXElements = (path, config, filePath, updatedTranslations) => {
 const processJSXText = (path, config, filePath, updatedTranslations) => {
   const value = path.node.value.trim();
   if (value && !isURL(value)) {
-    const parts = [];
-    let lastIndex = 0;
-    let hasVariables = false;
-
-    const regex = /\{([^\}]+)\}/g;
-    let match;
-    while ((match = regex.exec(value)) !== null) {
-      hasVariables = true;
-      const precedingText = value.slice(lastIndex, match.index).trim();
-      if (precedingText) {
-        parts.push(precedingText);
-      }
-      parts.push(`\${${match[1].trim()}}`);
-      lastIndex = match.index + match[0].length;
-    }
-
-    const remainingText = value.slice(lastIndex).trim();
-    if (remainingText) {
-      parts.push(remainingText);
-    }
-
-    if (hasVariables) {
-      const key = createTranslationKey(
-        config.componentsDir,
-        filePath,
-        parts.join(" ")
-      );
-      updatedTranslations[key] = parts
-        .join(" ")
-        .replace(/\$\{([^\}]+)\}/g, "${$1}");
-      const args = parts
-        .filter((part) => part.startsWith("${") && part.endsWith("}"))
-        .map((part) => {
-          const varName = part.slice(2, -1);
-          return t.objectProperty(t.identifier(varName), t.identifier(varName));
-        });
-
-      path.replaceWith(
-        t.jsxExpressionContainer(
-          t.callExpression(t.identifier("t"), [
-            t.stringLiteral(key),
-            t.objectExpression(args),
-          ])
-        )
-      );
-      console.log("...Updated innerText...");
-      return true;
-    } else {
-      const key = createTranslationKey(config.componentsDir, filePath, value);
-      updatedTranslations[key] = value;
-      path.replaceWith(
-        t.jsxExpressionContainer(
-          t.callExpression(t.identifier("t"), [t.stringLiteral(key)])
-        )
-      );
-      console.log("...Updated innerText...");
-      return true;
-    }
+    const textNode = t.stringLiteral(value);
+    const callExpression = processLiteralValue(textNode, config, filePath, updatedTranslations);
+    path.replaceWith(
+      t.jsxExpressionContainer(callExpression)
+    );
+    console.log("...Updated innerText...");
+    return true;
   }
   return false;
 };
